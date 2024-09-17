c021b1956ab6134f667ef3172ea980528b7c1cc0
Amir Asadpour <asadpour.ah@gmail.com>
2024-09-17 23:18:07 +0200

    Removed unnecessary theming files in favor of edge to edge capability

--- app/src/main/java/nl/appical/bookreader/presentation/main/MainActivity.kt
+++ app/src/main/java/nl/appical/bookreader/presentation/main/MainActivity.kt
@@ -3,13 +3,15 @@ package nl.appical.bookreader.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hi")
+            Text("HI")
         }
     }
 }
